3489629f438ab5c2fe6f8fd4f551d65b1b1ff098
lilibethBm <lilibeth.karen@gmail.com>
2019-09-25 19:23:07 -0500

    .

--- wp-content/plugins/visualcomposer/public/sources/assetsLibrary/backgroundVideoVimeo/src/backgroundVideoVimeo.js
+++
@@ -1,9 +0,0 @@
-import './backgroundVideoVimeo.css';
-
-window.vcv.on('ready', function (action, id) {
-  if (action !== 'merge') {
-    var selector = '[data-vce-assets-video-vimeo]';
-    selector = id ? '[data-vcv-element="' + id + '"] ' + selector : selector;
-    window.vceAssetsBackgroundVideoVimeo(selector);
-  }
-});
